7ad163cc2c814d4038d8fb999d08a53c6cc4a45e
klvsov <klvsov@gmail.com>
2020-09-30 21:28:55 +0300

    starting develop

---
+++ src/TodoItem.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+
+const TodoItem = ({title, id, completed}) => {
+  const [checked, setChecked] = useState(completed);
+
+  const classes = ['todo'];
+
+  if(checked) {
+    classes.push('completed');
+  }
+
+  return (
+    <li className={classes.join(' ')}>
+      <label>
+        <input
+          type = "checkbox"
+          checked = {checked}
+          onChange = {() => setChecked(checked => !checked)}
+        />
+        <span>{title}</span>
+
+        <i
+          className="material-icons red-text"
+        >
+          close
+        </i>
+      </label>
+    </li>
+  )
+}
+
+export default TodoItem;
